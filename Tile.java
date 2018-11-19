12cb61fa04808539d5d5ffdf13d8702bb0026649
Roy Springer <cyrielspringer@gmail.com>
2018-11-19 21:53:20 +0100

    Added new constructor with a type parameter

--- Tile.java
+++ Tile.java
@@ -8,12 +8,25 @@ public class Tile extends BasicTile {
 
     /**
      * Constructor of the tile. Creates a tile based on image, width and height
-     *a
+     *
      * @param image Path to the image file
      * @param width Width of the tile
      * @param heigth Height of the tile
      */
     public Tile(String image, int width, int heigth) {
         super(image, width, heigth);
+    }
+
+    /**
+     * Constructor of the tile. Creates a tile based on image, width, height and
+     * a tile type
+     *
+     * @param image Path to the image file
+     * @param width Width of the tile
+     * @param heigth Height of the tile
+     * @param type
+     */
+    public Tile(String image, int width, int heigth, TileType type) {
+        super(image, width, heigth, type);
     }
 }
